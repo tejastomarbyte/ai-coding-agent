26cb9601037988e368b4a6ad3af293e2e4ecad8c
Tejas Tomar <tejastomar@Tejass-Mac-mini.local>
2026-04-22 23:23:41 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,29 +7,56 @@ from openai import OpenAI
 API_KEY = os.getenv("OPENROUTER_API_KEY")
 BASE_URL = os.getenv("OPENROUTER_BASE_URL", default="https://openrouter.ai/api/v1")
 
-TOOLS = [{
-    "type": "function",
-    "function": {
-        "name": "Read",
-        "description": "Read and return the contents of a file",
-        "parameters": {
-            "type": "object",
-            "properties": {
-                "file_path": {
-                    "type": "string",
-                    "description": "The path to the file to read"
-                }
-            },
-            "required": ["file_path"]
+TOOLS = [
+    {
+        "type": "function",
+        "function": {
+            "name": "Read",
+            "description": "Read and return the contents of a file",
+            "parameters": {
+                "type": "object",
+                "properties": {
+                    "file_path": {
+                        "type": "string",
+                        "description": "The path to the file to read"
+                    }
+                },
+                "required": ["file_path"]
+            }
         }
-    }
-}]
+    },
+    {
+        "type": "function",
+        "function": {
+            "name": "Write",
+            "description": "Write content to a file",
+            "parameters": {
+                "type": "object",
+                "required": ["file_path", "content"],
+                "properties": {
+                    "file_path": {
+                        "type": "string",
+                        "description": "The path of the file to write to"
+                    },
+                    "content": {
+                        "type": "string",
+                        "description": "The content to write to the file"
+                    }
+                }
+            }
+        }
+    },
+]
 
 
 def execute_tool(name, args):
     if name == "Read":
         with open(args["file_path"], "r") as f:
             return f.read()
+    if name == "Write":
+        with open(args["file_path"], "w") as f:
+            f.write(args["content"])
+        return f"Written to {args['file_path']}"
     raise ValueError(f"Unknown tool: {name}")
 
 
